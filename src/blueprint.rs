1dc4479f2bceb3607fd77926f50f2e78d314a6ac
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-11-13 11:56:06 +0100

    clean

--- src/blueprint.rs
+++ src/blueprint.rs
@@ -36,6 +36,39 @@ impl<const STRATEGY: u8> VcbParser<STRATEGY> {
     }
 }
 
+
+/// contains the raw footer data.
+#[derive(Debug, Default)]
+#[repr(C)]
+struct Footer {
+    height_type: i32,
+    height: i32,
+    width_type: i32,
+    width: i32,
+    bytes_type: i32,
+    bytes: i32,
+    layer_type: i32,
+    layer: i32,
+}
+impl Footer {
+    const SIZE: usize = std::mem::size_of::<Self>();
+    fn from_bytes(bytes: [u8; Footer::SIZE]) -> FooterInfo {
+        let read_int = |i: usize| i32::from_le_bytes([0, 1, 2, 3].map(|k| bytes[k + (i * 4)]));
+        #[rustfmt::skip]
+        let footer = FooterInfo::new(&Footer {
+            height_type: read_int(0),
+            height:      read_int(1),
+            width_type:  read_int(2),
+            width:       read_int(3),
+            bytes_type:  read_int(4),
+            bytes:       read_int(5),
+            layer_type:  read_int(6),
+            layer:       read_int(7),
+        });
+        footer
+    }
+}
+
 /// Useable cleaned footer data.
 #[derive(Debug)]
 struct FooterInfo {
@@ -66,6 +99,251 @@ enum Layer {
     Off,
 }
 
+
+#[derive(Debug)]
+pub struct VcbBoard<const STRATEGY: u8> {
+    elements: Vec<BoardElement>,
+    nodes: Vec<BoardNode>,
+    pub(crate) network: GateNetwork<STRATEGY>,
+    pub(crate) compiled_network: CompiledNetwork<STRATEGY>,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl<const STRATEGY: u8> VcbBoard<STRATEGY> {
+    /// For regression testing
+    #[must_use]
+    pub fn make_state_vec(&self) -> Vec<bool> {
+        let mut a = Vec::new();
+        for i in 0..self.elements.len() {
+            a.push(match self.elements[i].id {
+                None => false,
+                Some(node_id) => match self.nodes[node_id].network_id {
+                    None => false,
+                    Some(id) => self.compiled_network.get_state(id),
+                },
+            });
+        }
+        a
+    }
+    #[must_use]
+    pub(crate) fn make_inner_state_vec(&self) -> Vec<bool> {
+        self.compiled_network.get_state_vec()
+    }
+    #[inline(always)]
+    pub fn update(&mut self) {
+        self.compiled_network.update();
+    }
+    fn new(data: &[u8], width: usize, height: usize, optimize: bool) -> Self {
+        let num_elements = width * height;
+        let mut elements = Vec::with_capacity(num_elements);
+
+        for i in 0..width * height {
+            elements.push(BoardElement::new(&data[i * 4..i * 4 + 4]));
+        }
+        let mut board = VcbBoard {
+            elements,
+            nodes: Vec::new(),
+            width,
+            height,
+            network: GateNetwork::default(),
+            compiled_network: CompiledNetwork::default(),
+        };
+        let mut counter = 0;
+        for x in 0..num_elements {
+            board.explore(x.try_into().unwrap(), &mut counter);
+        }
+        // add vertexes to network
+        for node in &mut board.nodes {
+            node.network_id = Some(board.network.add_vertex(node.kind));
+        }
+        // add edges to network
+        for i in 0..board.nodes.len() {
+            let node = &board.nodes[i];
+            for input in &node.inputs {
+                assert!(board.nodes[*input].outputs.contains(&i));
+            }
+            let mut inputs: Vec<usize> = node
+                .inputs
+                .clone()
+                .into_iter()
+                .map(|x| board.nodes[x].network_id.unwrap())
+                .collect();
+            inputs.sort_unstable();
+            inputs.dedup();
+            board
+                .network
+                .add_inputs(node.kind, node.network_id.unwrap(), inputs);
+        }
+        board.compiled_network = board.network.compiled(optimize);
+
+        // TODO: terminal buffer
+        board
+    }
+    fn add_connection(&mut self, connection: (usize, usize), swp_dir: bool) {
+        let (start, end) = if swp_dir {
+            (connection.1, connection.0)
+        } else {
+            connection
+        };
+        assert!(start != end);
+        let a = self.nodes[start].inputs.insert(end);
+        let b = self.nodes[end].outputs.insert(start);
+        assert!(a == b);
+        assert_ne!(
+            (self.nodes[start].kind == GateType::Cluster),
+            (self.nodes[end].kind == GateType::Cluster)
+        );
+    }
+
+    // floodfill with id at given index
+    // pre: logic trace, otherwise id could have been
+    // assigned to nothing, this_x valid
+    fn fill_id(&mut self, this_x: i32, id: usize) {
+        let this = &mut self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
+        let this_kind = this.kind;
+        assert!(this_kind.is_logic());
+        match this.id {
+            None => this.id = Some(id),
+            Some(this_id) => {
+                assert_eq!(this_id, id);
+                return;
+            },
+        }
+        let width: i32 = self.width.try_into().unwrap();
+        'side: for dx in [1, -1, width, -width] {
+            'forward: for ddx in [1, 2] {
+                //TODO: handle wrapping
+                let other_x = this_x + dx * ddx;
+                let other_kind =
+                    unwrap_or_else!(self.elements.get(other_x as usize), continue 'side).kind;
+                if other_kind == Trace::Cross {
+                    continue 'forward;
+                }
+                if !other_kind.is_same_as(this_kind) {
+                    continue 'side;
+                }
+                self.fill_id(other_x, id);
+                continue 'side;
+            }
+        }
+    }
+    // Connect gate with immediate neighbor
+    // pre: this is gate, this_x valid, this has id.
+    fn connect_id(&mut self, this_x: i32, this_id: usize, id_counter: &mut usize) {
+        let this = &mut self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
+        let this_kind = this.kind;
+        assert!(this_kind.is_gate());
+        assert!(this.id.is_some());
+
+        let width: i32 = self.width.try_into().unwrap();
+        'side: for dx in [1, -1, width, -width] {
+            //TODO: handle wrapping
+            let other_x = this_x + dx;
+            let other = unwrap_or_else!(self.elements.get(other_x as usize), continue 'side);
+
+            let dir = match other.kind {
+                Trace::Read => false,
+                Trace::Write => true,
+                _ => continue,
+            };
+            let other_id = unwrap_or_else!(other.id, { self.add_new_id(other_x, id_counter) });
+            self.add_connection((this_id, other_id), dir);
+        }
+    }
+
+    // pre: this trace is logic
+    fn add_new_id(&mut self, this_x: i32, id_counter: &mut usize) -> usize {
+        let this = &self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
+        assert!(this.kind.is_logic());
+        // create a new id.
+        self.nodes.push(BoardNode::new(this.kind));
+        let this_id = *id_counter;
+        *id_counter += 1;
+
+        // fill with this_id
+        self.fill_id(this_x, this_id);
+        this_id
+    }
+
+    // this HAS to be recursive since gates have arbitrary shapes.
+    fn explore(&mut self, this_x: i32, id_counter: &mut usize) {
+        // if prev_id is Some, merge should be done.
+        // don't merge if id already exists, since that wouldn't make sense
+
+        // all here is correct
+        let this = &self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
+        let this_kind = this.kind;
+
+        if !this.kind.is_logic() {
+            return;
+        }
+
+        let this_id = unwrap_or_else!(this.id, { self.add_new_id(this_x, id_counter) });
+
+        assert!(this_kind.is_logic());
+        if !this_kind.is_gate() {
+            return;
+        }
+
+        self.connect_id(this_x, this_id, id_counter);
+    }
+
+    pub fn print_marked(&self, marked: &[usize]) {
+        println!("\nBoard:");
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let i = x + y * self.width;
+                self.elements[i].print(self, i, marked.contains(&i), true);
+            }
+            println!();
+        }
+    }
+    fn print_inner(&self, debug: bool) {
+        println!("\nBoard:");
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let i = x + y * self.width;
+                self.elements[i].print(self, i, false, debug);
+            }
+            println!();
+        }
+    }
+    fn print_compact<E: std::convert::From<std::io::Error>>(&self) -> Result<(), E> {
+        let mut stdout = stdout();
+        stdout.queue(Print("\n"))?;
+        for y in (0..self.height).step_by(2) {
+            for x in 0..self.width {
+                let i = x + y * self.width;
+                let i2 = x + (y + 1) * self.width;
+                let col = self.elements[i].get_color(self);
+                let col2 = self
+                    .elements
+                    .get(i2)
+                    .map(|s| s.get_color(self))
+                    .unwrap_or(Trace::Empty.to_color_off());
+
+                stdout.queue(SetColors(Colors::new(
+                    (col2[0], col2[1], col2[2]).into(),
+                    (col[0], col[1], col[2]).into(),
+                )))?;
+                stdout.queue(Print("▄"))?;
+            }
+            stdout.queue(Print("\n"))?;
+        }
+        stdout.queue(ResetColor)?;
+        stdout.flush()?;
+        Ok(())
+    }
+
+    pub fn print_debug(&self) {
+        self.print_inner(true);
+    }
+    pub fn print(&self) {
+        //self.print_compact().unwrap();
+        self.print_debug();
+    }
+}
 
 /// All color constants used by vcb
 #[rustfmt::skip]
@@ -145,7 +423,6 @@ enum Trace {
     Annotation,
     Filler,
 }
-
 impl Trace {
     #[rustfmt::skip]
     fn to_color_raw(self) -> [u8; 4] {
@@ -418,279 +695,4 @@ impl BoardNode {
 //    height: usize,
 //}
 
-#[derive(Debug)]
-pub struct VcbBoard<const STRATEGY: u8> {
-    elements: Vec<BoardElement>,
-    nodes: Vec<BoardNode>,
-    pub(crate) network: GateNetwork<STRATEGY>,
-    pub(crate) compiled_network: CompiledNetwork<STRATEGY>,
-    pub width: usize,
-    pub height: usize,
-}
 
-impl<const STRATEGY: u8> VcbBoard<STRATEGY> {
-    /// For regression testing
-    #[must_use]
-    pub fn make_state_vec(&self) -> Vec<bool> {
-        let mut a = Vec::new();
-        for i in 0..self.elements.len() {
-            a.push(match self.elements[i].id {
-                None => false,
-                Some(node_id) => match self.nodes[node_id].network_id {
-                    None => false,
-                    Some(id) => self.compiled_network.get_state(id),
-                },
-            });
-        }
-        a
-    }
-    #[must_use]
-    pub(crate) fn make_inner_state_vec(&self) -> Vec<bool> {
-        self.compiled_network.get_state_vec()
-    }
-    #[inline(always)]
-    pub fn update(&mut self) {
-        self.compiled_network.update();
-    }
-    fn new(data: &[u8], width: usize, height: usize, optimize: bool) -> Self {
-        let num_elements = width * height;
-        let mut elements = Vec::with_capacity(num_elements);
-
-        for i in 0..width * height {
-            elements.push(BoardElement::new(&data[i * 4..i * 4 + 4]));
-        }
-        let mut board = VcbBoard {
-            elements,
-            nodes: Vec::new(),
-            width,
-            height,
-            network: GateNetwork::default(),
-            compiled_network: CompiledNetwork::default(),
-        };
-        let mut counter = 0;
-        for x in 0..num_elements {
-            board.explore(x.try_into().unwrap(), &mut counter);
-        }
-        // add vertexes to network
-        for node in &mut board.nodes {
-            node.network_id = Some(board.network.add_vertex(node.kind));
-        }
-        // add edges to network
-        for i in 0..board.nodes.len() {
-            let node = &board.nodes[i];
-            for input in &node.inputs {
-                assert!(board.nodes[*input].outputs.contains(&i));
-            }
-            let mut inputs: Vec<usize> = node
-                .inputs
-                .clone()
-                .into_iter()
-                .map(|x| board.nodes[x].network_id.unwrap())
-                .collect();
-            inputs.sort_unstable();
-            inputs.dedup();
-            board
-                .network
-                .add_inputs(node.kind, node.network_id.unwrap(), inputs);
-        }
-        board.compiled_network = board.network.compiled(optimize);
-
-        // TODO: terminal buffer
-        board
-    }
-    fn add_connection(&mut self, connection: (usize, usize), swp_dir: bool) {
-        let (start, end) = if swp_dir {
-            (connection.1, connection.0)
-        } else {
-            connection
-        };
-        assert!(start != end);
-        let a = self.nodes[start].inputs.insert(end);
-        let b = self.nodes[end].outputs.insert(start);
-        assert!(a == b);
-        assert_ne!(
-            (self.nodes[start].kind == GateType::Cluster),
-            (self.nodes[end].kind == GateType::Cluster)
-        );
-    }
-
-    // floodfill with id at given index
-    // pre: logic trace, otherwise id could have been
-    // assigned to nothing, this_x valid
-    fn fill_id(&mut self, this_x: i32, id: usize) {
-        let this = &mut self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
-        let this_kind = this.kind;
-        assert!(this_kind.is_logic());
-        match this.id {
-            None => this.id = Some(id),
-            Some(this_id) => {
-                assert_eq!(this_id, id);
-                return;
-            },
-        }
-        let width: i32 = self.width.try_into().unwrap();
-        'side: for dx in [1, -1, width, -width] {
-            'forward: for ddx in [1, 2] {
-                //TODO: handle wrapping
-                let other_x = this_x + dx * ddx;
-                let other_kind =
-                    unwrap_or_else!(self.elements.get(other_x as usize), continue 'side).kind;
-                if other_kind == Trace::Cross {
-                    continue 'forward;
-                }
-                if !other_kind.is_same_as(this_kind) {
-                    continue 'side;
-                }
-                self.fill_id(other_x, id);
-                continue 'side;
-            }
-        }
-    }
-    // Connect gate with immediate neighbor
-    // pre: this is gate, this_x valid, this has id.
-    fn connect_id(&mut self, this_x: i32, this_id: usize, id_counter: &mut usize) {
-        let this = &mut self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
-        let this_kind = this.kind;
-        assert!(this_kind.is_gate());
-        assert!(this.id.is_some());
-
-        let width: i32 = self.width.try_into().unwrap();
-        'side: for dx in [1, -1, width, -width] {
-            //TODO: handle wrapping
-            let other_x = this_x + dx;
-            let other = unwrap_or_else!(self.elements.get(other_x as usize), continue 'side);
-
-            let dir = match other.kind {
-                Trace::Read => false,
-                Trace::Write => true,
-                _ => continue,
-            };
-            let other_id = unwrap_or_else!(other.id, { self.add_new_id(other_x, id_counter) });
-            self.add_connection((this_id, other_id), dir);
-        }
-    }
-
-    // pre: this trace is logic
-    fn add_new_id(&mut self, this_x: i32, id_counter: &mut usize) -> usize {
-        let this = &self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
-        assert!(this.kind.is_logic());
-        // create a new id.
-        self.nodes.push(BoardNode::new(this.kind));
-        let this_id = *id_counter;
-        *id_counter += 1;
-
-        // fill with this_id
-        self.fill_id(this_x, this_id);
-        this_id
-    }
-
-    // this HAS to be recursive since gates have arbitrary shapes.
-    fn explore(&mut self, this_x: i32, id_counter: &mut usize) {
-        // if prev_id is Some, merge should be done.
-        // don't merge if id already exists, since that wouldn't make sense
-
-        // all here is correct
-        let this = &self.elements[TryInto::<usize>::try_into(this_x).unwrap()];
-        let this_kind = this.kind;
-
-        if !this.kind.is_logic() {
-            return;
-        }
-
-        let this_id = unwrap_or_else!(this.id, { self.add_new_id(this_x, id_counter) });
-
-        assert!(this_kind.is_logic());
-        if !this_kind.is_gate() {
-            return;
-        }
-
-        self.connect_id(this_x, this_id, id_counter);
-    }
-
-    pub fn print_marked(&self, marked: &[usize]) {
-        println!("\nBoard:");
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let i = x + y * self.width;
-                self.elements[i].print(self, i, marked.contains(&i), true);
-            }
-            println!();
-        }
-    }
-    fn print_inner(&self, debug: bool) {
-        println!("\nBoard:");
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let i = x + y * self.width;
-                self.elements[i].print(self, i, false, debug);
-            }
-            println!();
-        }
-    }
-    fn print_compact<E: std::convert::From<std::io::Error>>(&self) -> Result<(), E> {
-        let mut stdout = stdout();
-        stdout.queue(Print("\n"))?;
-        for y in (0..self.height).step_by(2) {
-            for x in 0..self.width {
-                let i = x + y * self.width;
-                let i2 = x + (y + 1) * self.width;
-                let col = self.elements[i].get_color(self);
-                let col2 = self
-                    .elements
-                    .get(i2)
-                    .map(|s| s.get_color(self))
-                    .unwrap_or(Trace::Empty.to_color_off());
-
-                stdout.queue(SetColors(Colors::new(
-                    (col2[0], col2[1], col2[2]).into(),
-                    (col[0], col[1], col[2]).into(),
-                )))?;
-                stdout.queue(Print("▄"))?;
-            }
-            stdout.queue(Print("\n"))?;
-        }
-        stdout.queue(ResetColor)?;
-        stdout.flush()?;
-        Ok(())
-    }
-
-    pub fn print_debug(&self) {
-        self.print_inner(true);
-    }
-    pub fn print(&self) {
-        //self.print_compact().unwrap();
-        self.print_debug();
-    }
-}
-
-/// contains the raw footer data.
-#[derive(Debug, Default)]
-#[repr(C)]
-struct Footer {
-    height_type: i32,
-    height: i32,
-    width_type: i32,
-    width: i32,
-    bytes_type: i32,
-    bytes: i32,
-    layer_type: i32,
-    layer: i32,
-}
-impl Footer {
-    const SIZE: usize = std::mem::size_of::<Self>();
-    fn from_bytes(bytes: [u8; Footer::SIZE]) -> FooterInfo {
-        let read_int = |i: usize| i32::from_le_bytes([0, 1, 2, 3].map(|k| bytes[k + (i * 4)]));
-        #[rustfmt::skip]
-        let footer = FooterInfo::new(&Footer {
-            height_type: read_int(0),
-            height:      read_int(1),
-            width_type:  read_int(2),
-            width:       read_int(3),
-            bytes_type:  read_int(4),
-            bytes:       read_int(5),
-            layer_type:  read_int(6),
-            layer:       read_int(7),
-        });
-        footer
-    }
-}
